abb7fa5496365fdf9e9aaccc86f9d0c2a5573d1f
Wildhoney <adam.timberlake@gmail.com>
2017-03-17 09:21:20 +0000

    Fixed issue with conditional

--- src/cli.js
+++ src/cli.js
@@ -9,8 +9,8 @@ const task = read().find(model => model.name === name);
 
 // Append the 'node_modules' location to the PATH for a single command only.
 const modules = './node_modules/.bin:$PATH';
-const command = task && isWin32 ? `cmd /V /C "set PATH=%path%;${modules} && ${task.tasks}`
-                                : `PATH=${modules} bash -c '${task.tasks}'`;
+const command = task && (isWin32 ? `cmd /V /C "set PATH=%path%;${modules} && ${task.tasks}`
+                                 : `PATH=${modules} bash -c '${task.tasks}'`);
 
 task ? spawn(command, args, { stdio: 'inherit', shell: true }) : (() => {
 
