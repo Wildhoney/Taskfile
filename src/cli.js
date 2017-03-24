5c863f8bf785dcb67d5c0fdc356641b7322fef26
Wildhoney <adam.timberlake@gmail.com>
2017-03-24 10:56:49 +0000

    Removed unnecessary CLI code

--- src/cli.js
+++ src/cli.js
@@ -1,28 +1,13 @@
 import { spawn } from 'child_process';
 import PrettyError from 'pretty-error';
-import { read, isWin32 } from './taskfile';
+import { read, exec } from './taskfile';
 import { list } from './help';
 
 // Take the name of the task, and then attempt to find the associated line in the YAML config.
 const [,, name = null] = process.argv;
 const task             = read().find(model => model.name === name);
 
-/**
- * @method run
- * @param {String} task
- * @return {void}
- */
-export const run = task => {
-
-    // Append the 'node_modules' location to the PATH for a single command only, and then spawn the selected task.
-    const [,,, ...args] = process.argv;
-    const binPath       = './node_modules/.bin';
-    const command       = isWin32 ? `cmd /V /C "set PATH=%path%;${binPath} && ${task}` : `PATH=${binPath}:$PATH bash -c '${task}'`;
-    spawn(command, args, { stdio: 'inherit', shell: true });
-
-};
-
-!name ? list() : task ? run(task.tasks) : (() => {
+!name ? list() : task ? exec(task) : (() => {
 
     // Render error that we're unable to find the desired task.
     const error = new PrettyError();
