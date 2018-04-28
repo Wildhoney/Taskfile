465f84c25b527a4403e7ec1b6787a1a729a790e3
Wildhoney <adam.timberlake@gmail.com>
2018-04-28 19:12:44 +0100

    Modified Webpack config for latest Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,16 @@
+const path = require('path');
+
 module.exports = {
     entry: './src/cli.js',
     target: 'node',
+    mode: process.env.NODE_ENV || 'development',
     output: {
         filename: './bin/index.js',
+        path: path.resolve('./'),
         libraryTarget: 'commonjs2',
     },
     module: {
-        loaders: [
+        rules: [
             {
                 test: /\.js$/,
                 loader: 'babel-loader',
